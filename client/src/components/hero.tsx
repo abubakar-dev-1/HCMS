a2bda2846bd17e7a7b50f6ca693b8f2189f4448a
abubakar-dev-1 <mianabubaka007@gmail.com>
2024-10-25 04:34:34 +0500

    final update

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -22,10 +22,10 @@ const Hero = () => {
       );
       const data = await res.json();
       const imageUrls = data.data[0]?.heroImages.map((imgObj: any) => {
-        console.log("images are ", imageUrls)
         return `${process.env.NEXT_PUBLIC_BASE_URL}${imgObj.url}`;
       });
-
+      
+      console.log("images are ", imageUrls)
 
       setImages(imageUrls || []);
     } catch (error) {
